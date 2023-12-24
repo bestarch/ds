6803f8e8706d70b5fb1a69dfc2b4e2274d8d60bd
~AB <srivastava07.abhishek@gmail.com>
2023-12-24 17:54:00 +0530

    Initial files

---
+++ src/test/java/com/bestarch/ds/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestarch.ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
